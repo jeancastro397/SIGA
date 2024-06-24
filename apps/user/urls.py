45dd41d6bcd1b9a8b01794d70d9c37ce9042e59f
NicEspinozaT <nic.espinozat@duocuc.cl>
2024-06-24 00:48:09 -0400

    Cerrar sesion implementado en backend

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from .views import (
-    login_view,
-    registro_view,
-    perfil
-)
+from .views import login_view, registro_view, perfil, cerrar_sesion
 
 
 urlpatterns = [
-    path('registrar/', registro_view, name='registrar' ),
-    path('login/', login_view, name='login'),
-    path('perfil/', perfil, name='perfil'),
+    path("registrar/", registro_view, name="registrar"),
+    path("login/", login_view, name="login"),
+    path("logout/", cerrar_sesion, name="logout"),
+    path("perfil/", perfil, name="perfil"),
 ]
